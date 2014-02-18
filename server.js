1482eb480fc38b67006e4440fb31e7fd626995e2
Andre Z. Sanchez <andrezacsanchez@gmail.com>
2014-02-18 07:55:53 -0500

    Make server.js mode modular
    By allowing it to be 'require'ed without automatically starting to listen.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 var express = require('express')
 var join = require('path').join
-var PORT = process.env.port || 80
 
 var routes = require('./routes')
 
@@ -18,10 +17,17 @@ function makeApp() {
   return app
 }
 
-var app = makeApp()
-app.listen(PORT, function(err) {
-  if (err) throw err
-  console.log('listening on port ' + PORT)
-})
+function startApp() {
+  var PORT = process.env.port || 80
+  var app = makeApp()
+  app.listen(PORT, function(err) {
+    if (err) throw err
+    console.log('listening on port ' + PORT)
+  })
+}
+
+if (module === require.main) {
+  startApp()
+}
 
 module.exports = makeApp
